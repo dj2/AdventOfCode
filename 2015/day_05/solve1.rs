26c266aa2296c01c867fc0f93fe52acad8258c8e
dan sinclair <dj2@everburning.com>
2021-09-15 20:37:42 -0400

    Add 2015 day 5

---
+++ 2015/day_05/solve1.rs
@@ -0,0 +1,71 @@
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("Missing input filename");
+    }
+
+    let filename = &args[1];
+    let data = std::fs::read_to_string(filename)
+        .expect(&format!("Unable to read {}", filename));
+
+    let nice: Vec<&str> = data.lines().filter(|l| check(l)).collect();
+    println!("{}", nice.len());
+}
+
+fn check(input: &str) -> bool {
+    let vowel_count = input.chars().filter(|c|
+            match c {
+                'a'|'e'|'i'|'o'|'u' => true,
+                _ => false,
+            }
+        ).collect::<Vec<char>>().len();
+
+    let bad_combo = input.contains("ab") ||
+            input.contains("cd") || input.contains("pq") ||
+            input.contains("xy");
+
+    let mut has_double_letter = false;
+    for (idx, c) in input.chars().enumerate() {
+        if idx == 0 {
+            continue;
+        }
+
+        if c == input.chars().nth(idx - 1).unwrap() {
+            has_double_letter = true;
+            break;
+        }
+    }
+
+    vowel_count >= 3 && !bad_combo && has_double_letter
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        assert!(check("ugknbfddgicrmopn"));
+    }
+
+    #[test]
+    fn ex2() {
+        assert!(check("aaa"));
+    }
+
+    #[test]
+    fn ex3() {
+        assert!(!check("jchzalrnumimnmhp"));
+    }
+
+    #[test]
+    fn ex4() {
+        assert!(!check("haegwjzuvuyypxyu"));
+    }
+
+    #[test]
+    fn ex5() {
+        assert!(!check("dvszwmarrgswjxmb"));
+    }
+}
---
+++ 2015/day_05/solve2.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("Missing input filename");
+    }
+
+    let filename = &args[1];
+    let data = std::fs::read_to_string(filename)
+        .expect(&format!("Unable to read {}", filename));
+
+    let nice: Vec<&str> = data.lines().filter(|l| check(l)).collect();
+    println!("{}", nice.len());
+}
+
+fn check(input: &str) -> bool {
+    let mut duplicate_pair = false;
+    let mut pairs: HashMap<String, usize> = HashMap::new();
+
+    let chars = input.as_bytes();
+    for idx in 0..(chars.len() - 1) {
+        let s = format!("{}{}", chars[idx], chars[idx + 1]);
+
+        match pairs.get(&s) {
+            Some(pos) => {
+                if idx > pos + 1 {
+                    duplicate_pair = true;
+                    break;
+                }
+            },
+            None => {
+                pairs.insert(s, idx);
+            },
+        }
+    }
+
+    let mut has_double_letter = false;
+    for (idx, c) in input.chars().enumerate() {
+        if idx == 0 || idx == 1 {
+            continue;
+        }
+
+        if c == input.chars().nth(idx - 2).unwrap() {
+            has_double_letter = true;
+            break;
+        }
+    }
+
+    duplicate_pair && has_double_letter
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        assert!(check("qjhvhtzxzqqjkmpb"));
+    }
+
+    #[test]
+    fn ex2() {
+        assert!(check("xxyxx"));
+    }
+
+    #[test]
+    fn ex3() {
+        assert!(!check("uurcxstgmygtbstg"));
+    }
+
+    #[test]
+    fn ex4() {
+        assert!(!check("ieodomkazucvgmuy"));
+    }
+}
+
