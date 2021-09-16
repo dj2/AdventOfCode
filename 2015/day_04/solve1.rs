fca1a641135ac8c5cd29c6838305c4540856dec3
dan sinclair <dj2@everburning.com>
2021-09-15 20:03:48 -0400

    Add 2015 day 04

---
+++ 2015/day_04/solve1.rs
@@ -0,0 +1,41 @@
+extern crate md5;
+
+fn main() {
+    println!("{}", calc("iwrupvqb"));
+}
+
+fn calc(input: &str) -> i32 {
+    let mut cur = 0;
+    loop {
+        let val = format!("{}{}", input, cur);
+        let digest = md5::compute(val.as_bytes());
+        let res = format!("{:x}", digest);
+
+        match res[0..5].parse::<i32>() {
+            Ok(v) => {
+                if v == 0 {
+                    break;
+                }
+            },
+            _ => {},
+        }
+        cur += 1;
+    }
+
+    cur
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        assert_eq!(calc("abcdef"), 609043);
+    }
+
+    #[test]
+    fn ex2() {
+        assert_eq!(calc("pqrstuv"), 1048970);
+    }
+}
---
+++ 2015/day_04/solve2.rs
@@ -0,0 +1,27 @@
+extern crate md5;
+
+fn main() {
+    println!("{}", calc("iwrupvqb"));
+}
+
+fn calc(input: &str) -> i32 {
+    let mut cur = 0;
+    loop {
+        let val = format!("{}{}", input, cur);
+        let digest = md5::compute(val.as_bytes());
+        let res = format!("{:x}", digest);
+
+        match res[0..6].parse::<i32>() {
+            Ok(v) => {
+                if v == 0 {
+                    break;
+                }
+            },
+            _ => {},
+        }
+        cur += 1;
+    }
+
+    cur
+}
+
